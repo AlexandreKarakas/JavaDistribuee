14e17e4eb208d36a1917f114b625800da7175321
AlexandreKarakas <alexandre.karakas@gmail.com>
2020-03-17 16:36:37 +0100

    modification

---
+++ Social_Network/src/Server.java
@@ -0,0 +1,62 @@
+import java.util.concurrent.*;
+import java.net.*;
+import java.io.*;
+
+public class Server {
+    private static int port;
+    private static int poolSize;
+    private boolean isFinished;
+    private ExecutorService pool;
+    private final ServerSocket serverSocket ;
+    public ExecutorService getPool() {
+        return pool;
+    }
+
+    public void setPool(ExecutorService pool) {
+        this.pool = pool;
+    }
+
+    public boolean isFinished() {
+        return isFinished;
+    }
+
+    public void setFinished(boolean isFinished) {
+        this.isFinished = isFinished;
+    }
+
+    public static int getPoolSize() {
+        return poolSize;
+    }
+
+    public static void setPoolSize(int poolSize) {
+        Server.poolSize = poolSize;
+    }
+
+    public static int getPort() {
+        return port;
+    }
+
+    public static void setPort(int port) {
+        Server.port = port;
+    }
+
+    public Server(int port, int poolSize) throws IOException {
+        serverSocket = new ServerSocket(port);
+        pool = Executors.newFixedThreadPool(poolSize);
+
+    }
+
+    public void manageRequest() {
+        try{
+            for(;;) {
+                pool.execute(new Slave(serverSocket.accept()));
+            }
+        }catch(IOException ex) {
+            pool.shutdown();
+        }
+    }
+
+    public Server() {
+
+    }
+}
---
+++ Social_Network/src/Slave.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class Slave {
+    private final Socket socket;
+    public Slave(Socket accept) {
+        this.socket = accept;
+    }
+    @Override
+    public void run() {
+        try {
+            InputStream in = socket.getInputStream();
+            OutputStream ou = socket.getOutputStream();
+            PrintWriter bos;
+            int i;
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String s = reader.readLine();
+            System.out.println(s);
+        }
+        catch (SocketException e) {
+            System.out.println(e);
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+        try {
+            socket.close();
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+
+    }
+
+}
