3aa822f7d82747534b598699ecd5ab3308cb9df4
AlexandreKarakas <alexandre.karakas@gmail.com>
2020-03-21 17:05:58 +0100

    modification

--- Social_Network/src/main.java
+++ Social_Network/src/main.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 public class main {
-    public static void main() {
+    public static void main(String[] args) {
         try {
             Server networkService = new Server(33333, 10);
             System.out.println("Serveur lance");
