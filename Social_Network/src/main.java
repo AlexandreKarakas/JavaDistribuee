5fdcd8907c9d45f9d9ec779bef628c748681c32e
AlexandreKarakas <alexandre.karakas@gmail.com>
2020-03-21 16:36:09 +0100

    add function lecture

--- Social_Network/src/main.java
+++ Social_Network/src/main.java
@@ -5,6 +5,7 @@ public class main {
             Server networkService = new Server(33333, 10);
             System.out.println("Serveur lance");
             networkService.manageRequest();
+            networkService.lecture();
         } catch (IOException e) {
             System.out.println(e);
         }
