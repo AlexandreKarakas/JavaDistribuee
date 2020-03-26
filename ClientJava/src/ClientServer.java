53542c22bfe9851e235068f2098a77df1cdb902d
AlexandreKarakas <alexandre.karakas@gmail.com>
2020-03-26 21:45:54 +0100

    modif

--- ClientJava/src/ClientServer.java
+++ ClientJava/src/ClientServer.java
@@ -1,3 +1,16 @@
-public class ClientServer {
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+public class ClientServer {
+    public static void main(String [] args) {
+        try{
+            Socket soc = new Socket("localhost", 3333);
+
+        }catch(UnknownHostException e){
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 }
